2c262588d6923dcf270f0d43e7f706b2806290c7
Vladimir Kuprienko <vldmr.kuprienko@gmail.com>
2016-12-24 13:46:40 +0200

    Variables, data types

--- lesson1/js/script.js
+++ lesson1/js/script.js
@@ -1,3 +1,20 @@
 // Output
-alert('Hello, world!');
+// alert('Hello, world!');
 console.log('Hello, World!');
+
+// Vars, data types
+var number = 2;
+var float = 2.5;
+var str = 'Hello';
+var bool = true;
+var obj = {};
+var undefined;
+var nullable = null;
+
+console.log(number + ' - ' + typeof number);
+console.log(float + ' - ' + typeof float);
+console.log(str + ' - ' + typeof str);
+console.log(bool + ' - ' + typeof bool);
+console.log(undefined + ' - ' + typeof undefined);
+console.log(obj + ' - ' + typeof obj);
+console.log(nullable + ' - ' + typeof nullable);
