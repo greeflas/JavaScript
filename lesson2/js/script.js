cbc8bd85cc395916711959570bec8a07fb21cc68
Vladimir Kuprienko <vldmr.kuprienko@gmail.com>
2017-01-21 15:17:08 +0200

    Removed comments

--- lesson2/js/script.js
+++ lesson2/js/script.js
@@ -1,71 +1,71 @@
 /* if-else, do-while */
-// var login = '';
-//
-// do {
-//     login = prompt('Input your login', '');
-//     if (login === null) {
-//         alert('Log in is by user exited...');
-//         break;
-//     }
-// } while(login !== 'admin');
-//
-// if (login === 'admin') {
-//     var password = prompt('Input your password', '');
-//
-//     var message = '';
-//     if (password === '123') {
-//         message = 'Welcome to dashboard!';
-//     }
-//     else {
-//         message = 'Password is wrong!';
-//     }
-//
-//     alert(message);
-// }
+var login = '';
+
+do {
+    login = prompt('Input your login', '');
+    if (login === null) {
+        alert('Log in is by user exited...');
+        break;
+    }
+} while(login !== 'admin');
+
+if (login === 'admin') {
+    var password = prompt('Input your password', '');
+
+    var message = '';
+    if (password === '123') {
+        message = 'Welcome to dashboard!';
+    }
+    else {
+        message = 'Password is wrong!';
+    }
+
+    alert(message);
+}
 
 /* while */
-// var n = +prompt('Input a number', '');
-// var power = +prompt('Input the power', '');
-//
-// var counter = 0;
-// var res = n;
-// while (++counter < power) {
-//     res *= n;
-// }
-//
-// alert('Result: ' + res);
+var n = +prompt('Input a number', '');
+var power = +prompt('Input the power', '');
+
+var counter = 0;
+var res = n;
+while (++counter < power) {
+    res *= n;
+}
+
+alert('Result: ' + res);
 
 /* for */
-// var n = +prompt('Input a number', '');
-// for (var i = 0; i < n; i++) {
-//     if (i % 4 == 0) {
-//         console.log(i);
-//     }
-// }
+var n = +prompt('Input a number', '');
+for (var i = 0; i < n; i++) {
+    if (i % 4 == 0) {
+        console.log(i);
+    }
+}
 
 /* break, continue */
-// var num = +prompt('Input a number', '');
-// for (var i = 0; i < num; i++) {
-//     if (i === 40) {
-//         break;
-//     }
-//     if (i % 4 == 0) {
-//         continue;
-//     }
-//
-//     console.log(i);
-// }
+var num = +prompt('Input a number', '');
+for (var i = 0; i < num; i++) {
+    if (i === 40) {
+        break;
+    }
+    if (i % 4 == 0) {
+        continue;
+    }
+
+    console.log(i);
+}
 
 /* division table */
-//var output = '';
-//for (var i = 2; i < 11; i++) {
-//    for (var j = 2; j < 11; j++) {
-//        output += (i * j) + '\t';
-//    }
-//    output += '\n';
-//}
-//
-//console.log(output);
+var output = '';
+for (var i = 2; i < 11; i++) {
+   for (var j = 2; j < 11; j++) {
+       output += (i * j) + '\t';
+   }
+   output += '\n';
+}
+
+console.log(output);
 
 /* Simple numbers */
 var n = prompt('Input a number', '');
