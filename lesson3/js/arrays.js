15b063d20994b3d6592a6054065d4021a41b5dc7
Vladimir Kuprienko <vldmr.kuprienko@gmail.com>
2017-01-28 14:00:35 +0200

    Arrays

--- lesson3/js/arrays.js
+++ lesson3/js/arrays.js
@@ -1,3 +1,19 @@
-/**
- * Created by student on 28.01.2017.
- */
+// var array = new Array(3); // create array with 3 elements with 'undefined' values
+var array = [];
+array[999] = 'test';
+
+console.log('Array length: ' + array.length);
+console.log('Array: ' + array);
+
+array.push('new element');
+console.log('Updated array: ' + array);
+
+var lastElement = array.pop();
+console.log(lastElement);
+
+// unshift, shift work's lowest then push, pop
+array.unshift('first element');
+console.log('Updated array: ' + array);
+
+var firstElement = array.shift();
+console.log(firstElement);
