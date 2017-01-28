720bf700d1c85222877c9a9036d7bca3dc18c370
Vladimir Kuprienko <vldmr.kuprienko@gmail.com>
2017-01-28 13:48:10 +0200

    Objects

---
+++ lesson3/js/arrays.js
@@ -0,0 +1,3 @@
+/**
+ * Created by student on 28.01.2017.
+ */
--- lesson3/js/script.js
+++ lesson3/js/script.js
@@ -1,5 +1,4 @@
 /* Default object methods */
-
 function findSubstrCount(str, subStr, pos, count) {
     pos = pos || 0;
     count = count || 0;
@@ -19,3 +18,37 @@ var string = 'test-blah-blah-test-test-blah';
 var subString = 'test';
 
 console.log('Result: ' + findSubstrCount(string, subString, 0));
+
+/* Objects */
+var person = {
+    firstName: "",
+    lastName: "",
+    age: undefined
+};
+
+console.log('Object: ' + person);
+console.log(person.age in person);
+
+function isEmpty(object) {
+    for (var key in object) {
+        return true;
+    }
+
+    return false;
+}
+
+var emptyObject = {};
+
+console.log(isEmpty(person));
+console.log(isEmpty(emptyObject));
+
+var phoneCodes = {
+    us: "1",
+    ua: "38",
+    ru: "7"
+};
+
+console.log("\nPhone codes:");
+for (var key in phoneCodes) {
+    console.log(phoneCodes[key]);
+}
