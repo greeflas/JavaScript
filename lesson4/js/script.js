a2170b3a7f2e0cb810f29a03f54eb695f456c075
Vladimir Kuprienko <vldmr.kuprienko@gmail.com>
2017-02-11 13:45:15 +0200

    DOM: selectors

--- lesson4/js/script.js
+++ lesson4/js/script.js
@@ -35,3 +35,37 @@ console.log(body.children);
 // check HTML element type
 console.log('<section> is instance of HTMLElement: ');
 console.log(body.children[0] instanceof HTMLElement);
+
+/* Selectors */
+// ID in HTMl == variable in JavaScript
+
+// return one element
+var byId = document.getElementById('message');
+console.log(byId);
+
+// return array of elements
+var byTag = document.getElementsByTagName('p');
+console.log(byTag);
+
+// return array of elements
+var byClass = document.getElementsByClassName('msg');
+console.log(byClass);
+
+// search only in document not in element
+// return array of elements
+var byName = document.getElementsByName('input');
+console.log(byName);
+
+// return all elements
+var all = document.getElementsByTagName('*');
+console.log(all);
+
+// get elements by CSS selector
+var bySelector = document.querySelector('section > div');
+console.log(bySelector);
+
+// check is equals CSS selector
+console.log(byId.matches('#message'));
+
+// return first parent with "body" selector
+console.log(byId.closest('body'));
