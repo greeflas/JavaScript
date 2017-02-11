22114c891603cb25437ac6e221eb85abbbdeb108
Vladimir Kuprienko <vldmr.kuprienko@gmail.com>
2017-02-11 14:17:04 +0200

    DOM: practice task 2

--- lesson4/js/practice.js
+++ lesson4/js/practice.js
@@ -1,6 +1,15 @@
+// Task 1
 var table = document.body.children[0];
 
 for (var i = 0; i < table.rows.length; i++) {
     table.rows[i].cells[i].style.backgroundColor = "black";
     table.rows[i].cells[i].style.color = "white";
+}
+
+// Task 2
+var li = document.getElementsByTagName('li');
+for (var i = 0; i < li.length; i++) {
+    var current = li[i].getElementsByTagName('li');
+    console.log("<li> value: " + li[i].firstChild.data.trim());
+    console.log("<li> count: " + current.length);
 }
