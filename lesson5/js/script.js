da6962b2e113de5aa65f05511d013e8fda762b1e
Vladimir Kuprienko <vldmr.kuprienko@gmail.com>
2017-03-04 13:06:59 +0200

    Scope

--- lesson5/js/script.js
+++ lesson5/js/script.js
@@ -7,3 +7,22 @@ function makeCounter() {
 
 var counter = makeCounter();
 // call counter() in console
+
+function timer() {
+    var counter = function () {
+        return counter.number++;
+    }
+    counter.number = 1;
+
+    counter.set = function (value) {
+        counter.number = value;
+    }
+
+    counter.reset = function () {
+        counter.number = 1;
+    }
+
+    return counter;
+}
+
+var myTimer = timer();
